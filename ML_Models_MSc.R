e6ac2130f8c99f1cb5ff4d1a23531393904a9ad7
olivierroncalez <olivier.roncalez@gmail.com>
2018-06-06 16:57:03 +0100

    Introduction code

--- ML_Models_MSc.R
+++ ML_Models_MSc.R
@@ -1,7 +1,21 @@
+# Run data cleaning script
 source('Data_Cleaning_MSc.R')
 
 
+# Loading required packages
+library(caret)
+library(doParallel) # Parallel computations
 
+
+# Parallelizing computations
+makeCluster(detectCores())
+registerDoParallel(15) # Change to acceptable number of cores based on your feasability
+getDoParWorkers() 
+stopCluster(cl) # Stop cluster computations
+
+
+
+# Basic Models ----------------------------------------------------------------------
 
 
 
